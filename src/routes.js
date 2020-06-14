b03d3303aa162482ccd542d7ec0766676a9d92a9
joaoianuci <joaoianuci@gmail.com>
2020-06-14 04:01:13 -0300

    Add authenticate route with auth middleware

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,8 @@ import FileController from './app/controllers/FileController';
 import LocationController from './app/controllers/LocationController';
 import ForgotPasswordController from './app/controllers/ForgotPasswordController';
 import ResetPasswordController from './app/controllers/ResetPasswordController';
+import AuthenticateController from './app/controllers/AuthenticateController';
+import authMiddleware from './middlewares/auth';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -19,6 +21,7 @@ routes.post(
 );
 routes.put(
   '/users/:user_id',
+  authMiddleware,
   upload.single('file'),
   UserController.update,
   LocationController.update,
@@ -26,9 +29,14 @@ routes.put(
 );
 
 routes.get('/users/:user_id', UserController.show);
-routes.delete('/users/:user_id', UserController.destroy);
+routes.delete('/users/:user_id', authMiddleware, UserController.destroy);
+routes.get('/debug-sentry', function mainHandler() {
+  throw new Error('My first Sentry error!');
+});
 
 routes.post('/forgot', ForgotPasswordController.store);
 routes.post('/reset', ResetPasswordController.store);
+
+routes.post('/users/authenticate', AuthenticateController.store);
 
 export default routes;
