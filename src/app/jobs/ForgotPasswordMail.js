1e90e96f08ca49252641f45a93279d655b470f15
joaoianuci <joaoianuci@gmail.com>
2020-06-14 03:50:33 -0300

    Create a job to send email to reset password

---
+++ src/app/jobs/ForgotPasswordMail.js
@@ -0,0 +1,21 @@
+import Mail from '../lib/Mail';
+
+export default {
+  key: 'ForgotPasswordMail',
+  options: {
+    priority: 1,
+  },
+  async handle({ data }) {
+    const { user, token } = data;
+
+    const text = `Hello ${user.name}`;
+
+    await Mail.sendMail({
+      to: user.email,
+      from: 'XyBarbers <xybarbers@xybarbers.com.br>',
+      template: 'forgot_password',
+      subject: 'Reset your password',
+      context: { text, token },
+    });
+  },
+};
