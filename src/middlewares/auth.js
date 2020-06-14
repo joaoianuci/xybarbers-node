c49b01fa795ff69ca768a3d7941cf9ad4cf67107
joaoianuci <joaoianuci@gmail.com>
2020-06-14 04:00:16 -0300

    Fix return error statement

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -5,7 +5,6 @@ export default (req, res, next) => {
   const authHeader = req.headers.authorization;
   if (!authHeader) return res.status(401).send({ error: 'No token provided' });
   const parts = authHeader.split(' ');
-
   if (!parts.length === 2)
     return res.status(401).send({ error: 'Token error' });
   const [scheme, token] = parts;
@@ -14,7 +13,7 @@ export default (req, res, next) => {
   jwt.verify(token, authConfig.secret, (err, decoded) => {
     if (err) return res.status(401).send({ error: err });
     req.userId = decoded.id;
-    return next();
+    return undefined;
   });
-  return res.status(500).send({ error: 'Problem with authorization' });
+  return next();
 };
