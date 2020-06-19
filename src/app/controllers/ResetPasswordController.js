cb52e7f078c3a28f0dd68cd4e330198fc9a314b6
joaoianuci <joaoianuci@gmail.com>
2020-06-18 23:15:21 -0300

    Fix: reset password broken authenticate user function

--- src/app/controllers/ResetPasswordController.js
+++ src/app/controllers/ResetPasswordController.js
@@ -1,11 +1,9 @@
 import * as Yup from 'yup';
-import bcrypt from 'bcryptjs';
 import User from '../models/User';
 
 class ResetPasswordController {
-  // eslint-disable-next-line consistent-return
   async store(req, res) {
-    const { token } = req.query;
+    const { token } = req.headers;
     const schema = Yup.object().shape({
       email: Yup.string().required(),
       password: Yup.string()
@@ -18,29 +16,27 @@ class ResetPasswordController {
     }
     try {
       const user = await User.findOne({ where: { email: req.body.email } });
-      if (!user) return res.status(404).send({ error: 'User not found' });
+      if (!user) return res.status(404).json({ error: 'User not found' });
       if (token !== user.password_reset_token)
-        return res.status(402).send({ error: 'Token invalid' });
+        return res.status(402).json({ error: 'Token invalid' });
       const now = new Date();
 
       if (now > user.password_reset_expires)
         return res
           .status(402)
-          .send({ error: 'Token expired, generate a new one' });
-
-      const hash = await bcrypt.hash(req.body.password, 8);
+          .json({ error: 'Token expired, generate a new one' });
 
       await user.update({
         password_reset_expires: null,
         password_reset_token: null,
-        password: hash,
+        password: req.body.password,
       });
     } catch (err) {
       return res
         .status(400)
-        .send({ error: 'Cannot reset password, try again' });
+        .json({ error: 'Cannot reset password, try again' });
     }
-    return res.send({ message: 'Password was reset with success' });
+    return res.json({ message: 'Password was reset with success' });
   }
 }
 export default new ResetPasswordController();
