83c78a9d46b9eb9ff9ad7a73a3164a375b50849a
joaoianuci <joaoianuci@gmail.com>
2020-06-18 23:25:13 -0300

    Create provider controller to validate providers signup

---
+++ src/app/controllers/ProviderValidateController.js
@@ -0,0 +1,27 @@
+import User from '../models/User';
+
+class ProviderValidateController {
+  async store(req, res) {
+    const { token } = req.headers;
+    const provider = await User.findByPk(req.userId, { provider: true });
+    if (!provider) {
+      return res.status(404).json({ message: 'The provider not exists' });
+    }
+    const { provider_validate, provider_token } = provider;
+    if (provider_validate) {
+      return res
+        .status(400)
+        .json({ message: 'This provider already validate' });
+    }
+    if (provider_token !== token) {
+      return res.status(402).json({ message: 'Token invalid' });
+    }
+    provider.update({ provider_validate: true, provider_token: null });
+    provider.save();
+
+    const filteredProvider = provider.filteredUser(provider);
+
+    return res.json(filteredProvider);
+  }
+}
+export default new ProviderValidateController();
