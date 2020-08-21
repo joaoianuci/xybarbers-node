d3c5857a7c452992c6dd881673e88c409fd87255
joaoianuci <joaoianuci@gmail.com>
2020-08-21 03:18:45 -0300

    Create: address table to managing user address

---
+++ src/database/migrations/20200821042104-create-addresses.js
@@ -0,0 +1,47 @@
+module.exports = {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.createTable('addresses', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'users', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      number: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      street: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      neighborhood: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      city: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      state: {
+        type: Sequelize.STRING(2),
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    }),
+  down: queryInterface => queryInterface.dropTable('addresses'),
+};
