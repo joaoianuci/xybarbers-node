d38bddcc60e967e873b962aececdbafbd09a3c22
joaoianuci <joaoianuci@gmail.com>
2020-08-21 03:22:22 -0300

    Create: address controller to CRUD operations

---
+++ src/app/controllers/AddressController.js
@@ -0,0 +1,30 @@
+import Address from '../models/Address';
+
+class AddressController {
+  async store(req, res, next) {
+    const { number, street, neighborhood, city, state } = req.body;
+    const { user } = req;
+    const address = await Address.create({
+      user_id: user.id,
+      number,
+      street,
+      neighborhood,
+      city,
+      state,
+    });
+    user.address_id = address.id;
+    await user.save();
+
+    return next();
+  }
+
+  async update(req, res, next) {
+    const { number, street, neighborhood, city, state } = req.body;
+    const { user } = req;
+    const address = await Address.findOne({ where: { user_id: user.id } });
+    address.update({ number, street, neighborhood, city, state });
+    return next();
+  }
+}
+
+export default new AddressController();
