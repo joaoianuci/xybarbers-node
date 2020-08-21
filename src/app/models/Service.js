979c363c091ce795b570189235f72ea6b5918287
joaoianuci <joaoianuci@gmail.com>
2020-08-21 03:34:14 -0300

    Create: service model to database managament

---
+++ src/app/models/Service.js
@@ -0,0 +1,23 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Service extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        description: Sequelize.STRING,
+        price: Sequelize.FLOAT,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
+  }
+}
+
+export default Service;
