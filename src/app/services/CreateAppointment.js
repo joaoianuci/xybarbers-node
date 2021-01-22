db7d045c12724d7832ccb2038e64d865ae67f841
joaoianuci <joaoianuci@gmail.com>
2021-01-21 22:05:27 -0300

    add: File, Address and Location model to user query

--- src/app/services/CreateAppointment.js
+++ src/app/services/CreateAppointment.js
@@ -2,6 +2,10 @@ import { startOfHour, parseISO, isBefore, format } from 'date-fns';
 import { enAU } from 'date-fns/locale';
 
 import User from '../models/User';
+import File from '../models/File';
+import Location from '../models/Location';
+import Address from '../models/Address';
+
 import Appointment from '../models/Appointment';
 import Notification from '../schemas/Notification';
 import Service from '../models/Service';
@@ -9,7 +13,7 @@ import Service from '../models/Service';
 class CreateAppointment {
   async run({ provider_id, user_id, service_id, date, res }) {
     if (provider_id === user_id) {
-      res
+    return res
         .status(400)
         .json({ error: 'You cannot create appointments for yourself' });
     }
@@ -18,24 +22,24 @@ class CreateAppointment {
     });
 
     if (!isProvider) {
-      res
+      return res
         .status(400)
         .json({ error: 'You can only create appointments with providers' });
     }
     const service = await Service.findByPk(service_id);
     if (!service) {
-      res.status(400).json({
+      return res.status(400).json({
         error: 'You can only create appointments with a valid service',
       });
     }
     if (!isProvider) {
-      res
+      return res
         .status(400)
         .json({ error: 'You can only create appointments with providers' });
     }
     const hourStart = startOfHour(parseISO(date));
     if (isBefore(hourStart, new Date())) {
-      res.status(400).json({ error: 'Past dates are not permitted' });
+      return res.status(400).json({ error: 'Past dates are not permitted' });
     }
     const checkAvailability = await Appointment.findOne({
       where: {
@@ -45,7 +49,7 @@ class CreateAppointment {
       },
     });
     if (checkAvailability) {
-      res.status(400).json({ error: 'Appointment date is not available' });
+      return res.status(400).json({ error: 'Appointment date is not available' });
     }
     const appointment = await Appointment.create({
       user_id,
@@ -54,18 +58,45 @@ class CreateAppointment {
       date,
     });
 
-    const user = await User.findByPk(user_id);
+   const user = await User.findByPk(user_id, {
+      attributes: {
+        exclude: [
+          'password_hash',
+          'password_reset_token',
+          'password_reset_expires',
+        ],
+      },
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+        {
+          model: Location,
+          as: 'point',
+          attributes: ['id', 'longitude', 'latitude'],
+        },
+        {
+          model: Address,
+          as: 'address',
+          attributes: ['id', 'number', 'street', 'neighborhood', 'city', 'state'],
+        }
+      ],
+    });
     const formattedDate = format(
       hourStart,
       "'day' dd 'of' MMMM', at' H:mm'h'",
       { locale: enAU }
     );
-
     await Notification.create({
       content: `A '${service.name}' service was scheduled by ${user.name} at ${formattedDate}.`,
-      user: provider_id,
+      user: {
+        id: provider_id,
+        name: user.name,
+        avatar: user.avatar.url,
+      },
     });
-
     return { appointment };
   }
 }
